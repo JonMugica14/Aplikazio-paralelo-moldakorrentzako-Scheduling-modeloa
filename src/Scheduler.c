c7d4a544fe5592f0bf44701614d2b43f00862b3c
Jon Mugica <ehujonmugica@gmail.com>
2024-07-05 10:10:30 +0200

    He creado el main y tal pero no tiene nada metido

--- src/Scheduler.c
+++ src/Scheduler.c
@@ -4,22 +4,21 @@
 #include <unistd.h>
 #include <string.h>
 #include "../include/variables.h"
-
+#include "../include/Scheduler.h"
 
 int time = 0;
 
-
 int scheduler()
 {
-    while(1)
+    while (1)
     {
-        if(num_jobs != 0 && job_queue[0].arrival_time == time && job_queue[0].events[0].num_cores <= free_cores)
+        if (num_jobs != 0 && job_queue[0].arrival_time == time && job_queue[0].events[0].num_cores <= free_cores)
         {
             int i = 0;
             int core = 0;
-            while(core < job_queue[0].events[0].num_cores)
+            while (core < job_queue[0].events[0].num_cores)
             {
-                if(cores[i].busy == 0)
+                if (cores[i].busy == 0)
                 {
                     cores[i].job = job_queue[0];
                     cores[i].busy = 1;
@@ -29,23 +28,38 @@ int scheduler()
                 i++;
             }
             free_cores -= job_queue[0].events[0].num_cores;
-            for(i = 0; i < num_jobs - 1; i++)
+            for (i = 0; i < num_jobs - 1; i++)
             {
                 job_queue[i] = job_queue[i + 1];
             }
             num_jobs--;
             num_scheduler_events++;
             i = 0;
-            while(i>=0 && job_queue[0].events[0].time <= scheduler_events[i].time)
+            while (i >= 0 && job_queue[0].events[0].time <= scheduler_events[i].time)
             {
                 scheduler_events[i + 1] = scheduler_events[i];
                 i--;
             }
-            scheduler_events[i + 1].jobs[num_jobs] = job_queue;
+            scheduler_events[i + 1].jobs[num_jobs] = job_queue[0];
             scheduler_events[i + 1].time = job_queue[0].events[0].time;
-
         }
 
-        if()
+        for (int i = 0; i < num_cores; i++)
+        {
+
+            if (cores[i].busy == 1)
+            {
+                if (cores[i].time == 0)
+                {
+
+                    free_cores += cores[i].job.events[0].num_cores;
+                    cores[i].busy = 0;
+                }
+                else
+                {
+                    cores[i].time--;
+                }
+            }
+        }
     }
 }
---
+++ src/main.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <string.h>
+#include "../include/variables.h"
+#include "../include/Scheduler.h"
+
+
+
+
+void initialize(){
+
+
+
+
+
+}
+
+
+
+
+int main(int argc, char *argv[])
+{
+
+
+    return 0;
+
+
+
+}
